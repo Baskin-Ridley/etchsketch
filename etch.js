4b449145e55750c1d1d5008a4b79e6ecef3b8f52
IPierrot123 <gbaskinridley@gmail.com>
2022-04-11 03:58:51 -0700

    Created the grid

--- etch.js
+++ etch.js
@@ -1,1 +1,12 @@
 console.log("check")
+
+let board = document.querySelector(".board");
+board.style.gridTemplateColumns = "repeat(16, 1fr)";
+board.style.gridTemplateRows = "repeat(16, 1fr)";
+
+for (let i = 0; i < 256; i++) {
+  let square = document.createElement("div");
+  square.style.backgroundColor = "blue";
+  board.insertAdjacentElement("beforeend", square);
+
+}

fdfe0bb5f674c0bc8db75e1ed1d8e718b05da6db
IPierrot123 <gbaskinridley@gmail.com>
2022-04-12 07:26:26 -0700

    Added a hover function

--- etch.js
+++ etch.js
@@ -1,12 +1,17 @@
 console.log("check")
 
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
+function createGrid (size){ 
+  let board = document.querySelector(".board");
+  board.style.gridTemplateColumns = "repeat(16, 1fr)";
+  board.style.gridTemplateRows = "repeat(16, 1fr)";
 
-for (let i = 0; i < 256; i++) {
-  let square = document.createElement("div");
-  square.style.backgroundColor = "blue";
-  board.insertAdjacentElement("beforeend", square);
-
+  for (let i = 0; i < 256; i++) {
+    let square = document.createElement("div");
+    square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = "yellow";
+    })
+    board.insertAdjacentElement("beforeend", square);
+  }
 }
+
+createGrid(16)
